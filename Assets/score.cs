a05489a89eb025aa944dd40423fc21eca3930f4f
ayesen <wx566@nyu.edu>
2019-10-15 15:02:40 -0400

    new scene

---
+++ Assets/score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class score : MonoBehaviour
+{
+    public int newscore;
+    public TextMeshProUGUI scoretext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        newscore = groundTrigger.me.score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoretext.text = "Final Score: " + newscore.ToString();
+    }
+}
--- Assets/scripts/groundTrigger.cs
+++ Assets/scripts/groundTrigger.cs
@@ -2,22 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class groundTrigger : MonoBehaviour
 {
     public int score = 0;
-    public Text scoretxt;
-    
+    public float timer = 60;
+
+    public TextMeshProUGUI scoretxt;
+    public TextMeshProUGUI timertxt;
+
+    static public groundTrigger me;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        me = this;
     }
 
     // Update is called once per frame
     void Update()
     {
-        scoretxt.text = "Your Score: "+score.ToString();
+        timer -= Time.deltaTime;
+        scoretxt.text = "Your Score: "+ score.ToString();
+        timertxt.text = "Time Left: " + (Mathf.RoundToInt(timer)).ToString();
+        
+        if(timer <= 0)
+        {
+            SceneManager.LoadScene("end");
+        }
+        
     }
 
     private void OnTriggerEnter(Collider other)
